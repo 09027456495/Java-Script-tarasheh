d4e90c4173c35a2475f6efbc394907029e02ae5a
amir <hasanoa68@gmail.com>
2022-08-23 23:32:41 +0430

    half 10

---
+++ 10/home/OurCode.js
@@ -0,0 +1,20 @@
+function my_Star_Fn1(){
+    $('.Our_star').css({"color":"black"});
+    $('#s1').css({"color":"yellow"});
+}
+function my_Star_Fn2(){
+    $('.Our_star').css({"color":"black"});
+    $('#s1,#s2').css({"color":"yellow"});
+}
+function my_Star_Fn3(){
+    $('.Our_star').css({"color":"black"});
+    $('#s1,#s2,#s3').css({"color":"yellow"});
+}
+function my_Star_Fn4(){
+    $('.Our_star').css({"color":"black"});
+    $('#s1,#s2,#s3,#s4').css({"color":"yellow"});
+}
+function my_Star_Fn5(){
+    $('.Our_star').css({"color":"black"});
+    $('.Our_star').css({"color":"yellow"});
+}
