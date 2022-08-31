804f18e89757becb71ac701a149c80e020fe50a0
amir <hasanoa68@gmail.com>
2022-08-31 19:00:25 +0430

    end of 12

--- 10/home/OurCode.js
+++ 10/home/OurCode.js
@@ -1,20 +1,40 @@
-function my_Star_Fn1(){
-    $('.Our_star').css({"color":"black"});
-    $('#s1').css({"color":"yellow"});
+function my_Star_Fn1() {
+    $('.show_result').fadeIn(1200).html(`یک ستاره`).addClass('fn_class_on');
+    setTimeout(() => {
+        $('.show_result').fadeOut(2300)
+    }, 2000);
+    $('.Our_star').css({ "color": "black" });
+    $('#s1').css({ "color": "yellow" });
 }
-function my_Star_Fn2(){
-    $('.Our_star').css({"color":"black"});
-    $('#s1,#s2').css({"color":"yellow"});
+function my_Star_Fn2() {
+    $('.show_result').fadeIn(3000).html(`دو ستاره`).addClass('fn_class_on');
+    setTimeout(() => {
+        $('.show_result').fadeOut(2300)
+    }, 2000);
+    $('.Our_star').css({ "color": "black" });
+    $('#s1,#s2').css({ "color": "yellow" });
 }
-function my_Star_Fn3(){
-    $('.Our_star').css({"color":"black"});
-    $('#s1,#s2,#s3').css({"color":"yellow"});
+function my_Star_Fn3() {
+    $('.show_result').fadeIn(3000).html(`سه ستاره`).addClass('fn_class_on');
+    setTimeout(() => {
+        $('.show_result').fadeOut(2300)
+    }, 2000);
+    $('.Our_star').css({ "color": "black" });
+    $('#s1,#s2,#s3').css({ "color": "yellow" });
 }
-function my_Star_Fn4(){
-    $('.Our_star').css({"color":"black"});
-    $('#s1,#s2,#s3,#s4').css({"color":"yellow"});
+function my_Star_Fn4() {
+    $('.show_result').fadeIn(3000).html(`چهار ستاره`).addClass('fn_class_on');
+    setTimeout(() => {
+        $('.show_result').fadeOut(2300)
+    }, 2000);
+    $('.Our_star').css({ "color": "black" });
+    $('#s1,#s2,#s3,#s4').css({ "color": "yellow" });
 }
-function my_Star_Fn5(){
-    $('.Our_star').css({"color":"black"});
-    $('.Our_star').css({"color":"yellow"});
+function my_Star_Fn5() {
+    $('.show_result').fadeIn(3000).html(`پنج ستاره`).addClass('fn_class_on');
+    setTimeout(() => {
+        $('.show_result').fadeOut(2300)
+    }, 2000);
+    $('.Our_star').css({ "color": "black" });
+    $('.Our_star').css({ "color": "yellow" });
 }
